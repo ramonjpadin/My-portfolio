67e4bfd517d0fff96c774c85577a36fc5cd7580a
Ramon Padin <ramonjpadin@gmail.com>
2022-09-01 13:30:02 -0500

    progress

--- script.js
+++ script.js
@@ -2,22 +2,48 @@
 var generateBtn = document.querySelector("#generate");
 
 
-function generatePassword() {
-    
+function generatePassword(lenght, upper, lower, numb,charcter); => {
+    let password =[];
+
+    if 
 
     return "Generated password will go here"
 }
-
+// random generators 
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const specialChar = ["!","@","#","$","%","^","&","*","(",")"];
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
+const passwordLength = 128;
+var password = "";
 function getRandomLower () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
+    var lowerCase = Math.floor(Math.random(26) * lowerCase.length);
 }
 function getRandomUpper () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
+    var upperCase = Math.floor(Math.random(26) * upperCase.length);
 }
-console.log(getRandomUpper());
-console.log(getRandomLower());
+function getRandomChar () {
+    var specialChar = Math.floor(Math.random(10) * specialChar.length);
+}
+function getRandomNum () {
+    var numbers = Math.floor(Math.random(10) * numbers.length);
+}
+
+console.log(getRandomChar);
+console.log(getRandomLower);
+console.log(getRandomNum);
+console.log(getRandomUpper)
+
+
+
+//maximum length of password validation
+if(passwordLength < 8) {  
+}
+   
+ if(passwordLength > 128) {    
+ } 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
